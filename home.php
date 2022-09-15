0d9c25fd35debc4cf481194cc53ec9f18d4c3991
Ana Stojic <as20201042@student.fon.bg.ac.rs>
2022-09-15 13:02:30 +0200

    Inicijalizovan kostur projekta

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/druga.css">
+    <title>Jugoslovensko dramsko pozorište</title>
+</head>
+<body>
+<h1 id="naslov">Prikazivanje predstava u JDP-u</h1>  
+<div class="zaglavlje">
+      <ul class="pozadina">
+        <li><button id="btn-prikazi">Prikaži ceo plan repertoara</button></li>
+        <li><button id="btn-pretraga">Pretraži predstavu</button></li>
+        <li><button id="btn-zakazivanje">Zakaži prikazivanje prestave</button></li>
+        <li><button id="btn-obrisi">Obriši sve predstave</button></li>
+       
+      
+         </ul>
+</div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Pozorište</title>
+</head>
+<body>
+<div class="login-form">
+        <div class="main-div">
+            <form method="POST" action="#">
+                <h1 id="naslov">Jugoslovensko dramsko Pozorište</h1>
+                <div class="forma">
+                    <label id="ime">Korisničko ime</label>
+                    <input type="text" name="username" class="polje"  required>
+                    <br>
+                    <br>
+                    <br>
+                    <label id="sifra">Lozinka</label>
+                    <input type="password" name="password" class="polje" required>
+                    <br>
+                    <br>
+                    <br>
+                    <button type="submit" id="dugme" name="submit">Prijavi se</button>
+                    
+                </div>
+</body>
+</html>
